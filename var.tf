9f307f842d5630271990137330a236e49b81afd6
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-11 18:48:59 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "secretkey" {
+    type = string
+  }
+
+  variable "accesskey" {
+    type = string
+    
+  }
+
+  variable "region" {
+    type = string
+    default = "ap-southeast-2"
+    
+  }
