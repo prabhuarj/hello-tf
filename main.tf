94be63ea9986e28b135e635ecce2370e92e72d48
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-11 20:34:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "subnet2" {
     cidr_block = "192.168.1.0/24"
     availability_zone = "ap-southeast-2b"
     tags = {
-      Name = "subnet-1"
+      Name = "subnet-2"
     }
     vpc_id = "${aws_vpc.myvpc.id}"
 }
