d5e78a5c6de851df0ce9186085ccf133ffddf08a
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-11 20:29:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnet1"{
 }
 
 resource "aws_subnet" "subnet2" {
-    cidr_block = 192.168.1.9/24
+    cidr_block = 192.168.1.0/24
     availability_zone = "ap-southeast-2b"
     tags = {
       Name = "subnet-1"
