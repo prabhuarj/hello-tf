33c348881c38a8aedbcd71d730ae69e2c9175dd3
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-12 10:17:56 +0530

    Create extend-network.tf

---
+++ extend-network.tf
@@ -0,0 +1,18 @@
+resource "aws_internet_gateway" "IGW"
+    vpc_id = "${aws_vpc.myvpc.id}"
+    tags = {
+         Name = "from tf
+    }
+    
+resource "aws_route_table" "publicrt" {
+    vpc_id = "${aws_vpc.myvpc.id}"
+
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.IGW.id}"
+    }
+
+    tags = {
+        "Name" = "public rt-tf"
+    }
+}
