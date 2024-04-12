5a5411fbfd8af84fe1b44cd132ab2514b98e9f90
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-12 10:28:29 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -12,3 +12,25 @@ variable "secretkey" {
     default = "ap-southeast-2"
     
   }
+
+  variable "subnet1cidr" {
+    type = string
+    default = "192.168.0.0/24"
+ 
+  }
+
+  variable "subnet2cidr" {
+    type = string
+    default = "192.168.1.0/24"
+  }
+
+  variable "subnet3cidr" {
+    type = string
+    default = "192.168.2.0/24"
+  }
+
+  variable "vpc-cidr" {
+    type = string
+    default = "192.168.0.0/16"
+  }
+
